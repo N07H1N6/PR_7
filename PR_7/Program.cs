fdc790a2e2a436bc31b1269b466aa5b8bf558f2a
Zer0 <53601161+N07H1N6@users.noreply.github.com>
2022-11-09 16:11:33 +0300

    Add files via upload

---
+++ PR_7/Program.cs
@@ -0,0 +1,32 @@
+﻿//************************************************************************
+//*Практическая работа № 7                                             *
+//* Выполнил:Кондаков.П.А,Группа 2ИСП                                    *
+//*Задание:Составление программ циклической структуры: цикл с параметром.*
+//************************************************************************
+
+using System;
+
+namespace PR_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b,h,F,x;
+            Console.WriteLine("Здравствуйте");
+            Console.WriteLine("Введите значения");
+            Console.Write("a= ");
+            Console.Write("b= ");
+            Console.Write("h= ");
+            Console.Wr("x= ");
+            Console.WriteLine("вычисление значений функции F(x) на отрезке [а, b] с шагом h.");
+            for( )
+
+            {
+                F = Math.Tan(x / 2) + (2 * Math.Cos(x));
+                Console.Write(F + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
