a13559571c7330d462694b5bec2048d23f652741
Zer0 <53601161+N07H1N6@users.noreply.github.com>
2022-11-24 21:54:08 +0300

    Add files via upload

--- PR_7/Program.cs
+++ PR_7/Program.cs
@@ -12,21 +12,36 @@ namespace PR_7
     {
         static void Main(string[] args)
         {
-            double a, b,h,F,x;
+            double a, b, h, F, x;
             Console.WriteLine("Здравствуйте");
             Console.WriteLine("Введите значения");
-            Console.Write("a= ");
-            Console.Write("b= ");
-            Console.Write("h= ");
-            Console.Wr("x= ");
-            Console.WriteLine("вычисление значений функции F(x) на отрезке [а, b] с шагом h.");
-            for( )
-
+            Console.WriteLine("a= ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("b= ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("h= ");
+            h = Convert.ToDouble(Console.ReadLine());
+            for (x = a; x <= b; x = x + h)
             {
-                F = Math.Tan(x / 2) + (2 * Math.Cos(x));
-                Console.Write(F + " ");
+                Console.Write($"F = {x}");
+
+                Console.Write($"\nЗначение функции при  F={x}\t" + Math.Tan(x / 2) + (2 * Math.Cos(x)));
             }
             Console.ReadKey();
         }
+
     }
 }
+            
+            
+
+            
+
+
+
+
+
+
+
+
+        
--- PR_7/Properties/AssemblyInfo.cs
+++ PR_7/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("PR_7")]
+[assembly: AssemblyTitle("PR_76")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PR_7")]
+[assembly: AssemblyProduct("PR_76")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
-[assembly: Guid("36bd31d8-49b6-4d0f-a67d-9bb3010c3dc4")]
+[assembly: Guid("04e93cb4-8794-4ebd-95bc-0fea069e4dca")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
