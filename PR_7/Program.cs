7716362fe444287cb80164e3a3557ceb755b0f0e
Zer0 <53601161+N07H1N6@users.noreply.github.com>
2022-11-24 22:19:42 +0300

    Add files via upload

--- PR_7/Program.cs
+++ PR_7/Program.cs
@@ -12,20 +12,37 @@ namespace PR_7
     {
         static void Main(string[] args)
         {
-            double a, b, h, F, x;
-            Console.WriteLine("Здравствуйте");
-            Console.WriteLine("Введите значения");
-            Console.WriteLine("a= ");
-            a = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("b= ");
-            b = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("h= ");
-            h = Convert.ToDouble(Console.ReadLine());
-            for (x = a; x <= b; x = x + h)
+            try
             {
-                Console.Write($"F = {x}");
+                double a, b, h, F, x;
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Здравствуйте");
+                Console.WriteLine("Введите значения");
+                Console.Write("a= ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("b= ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.Write("h= ");
+                h = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write($"\nЗначение функции при  F={x}\t" + Math.Tan(x / 2) + (2 * Math.Cos(x)));
+                for (x = a; x <= b; x = x + h)
+                {
+                    Console.Write($"F = {x}");
+
+                    Console.Write($"\nЗначение функции при  F={x}\t" + Math.Tan(x / 2) + (2 * Math.Cos(x)));
+                    Console.Write(" ");
+                }
+            }
+            catch(DivideByZeroException e)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Пользователь ввел ноль:" + e.Message);
+            }
+            catch (Exception fe)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка:" + fe.Message);
             }
             Console.ReadKey();
         }
